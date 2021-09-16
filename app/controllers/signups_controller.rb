85bdcd890944ae046af251e66efdaaa5a1aee25f
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-16 08:37:15 -0400

    Completed Assignment

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,15 @@
 class SignupsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+  def create
+    signup = Signup.create!(time: params[:time], camper_id: params[:camper_id], activity_id: params[:activity_id])
+    activity = signup.activity
+    render json: activity, status: :created
+  end
+
+private
+
+  def render_invalid_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
